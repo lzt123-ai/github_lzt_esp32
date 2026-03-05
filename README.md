ac429134c17fdda0dab620f62ff3d6c3b9ba7b3b
unknown <3558224977@qq.com>
2026-03-05 22:01:07 +0800

    完成第一个版本：创建helloworld程序

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# github_lzt_esp32
-programs
-this is my first commit
-增加了input输入功能
+#这是我的第一个版本
--- ex01/helloworld.py
+++ ex01/helloworld.py
@@ -1,4 +1,1 @@
-# 获取用户输入
-user_input = input("请输入内容: ")
-# 显示输出
-print("你输入的内容是:", user_input)
+print("hello,world")
