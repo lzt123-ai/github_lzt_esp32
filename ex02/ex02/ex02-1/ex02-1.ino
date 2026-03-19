d978a7bfa2427a679c491a90a03bcef810364ca6
unknown <3558224977@qq.com>
2026-03-20 01:18:16 +0800

    ex02-1初始化引脚设置

---
+++ ex02/ex02/ex02-1/ex02-1.ino
@@ -0,0 +1,17 @@
+// ex03 代码 - 第一次提交
+const int ledPin = 2;
+
+// 定义闪烁时长（毫秒）
+const int dotDuration = 200;   // 短闪：嘀
+const int dashDuration = 600;  // 长闪：哒
+const int gapDuration = 200;   // 闪与闪之间的间隔
+const int letterGap = 1000;    // 字母间隔
+const int endPause = 2000;     // 结束长暂停
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  // 稍后在此处编写 SOS 逻辑
+}
